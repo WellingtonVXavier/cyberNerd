5e2431940d811edda67533839a1698fd34022e77
wellington.xavier <sidious.lords@gmail.com>
2023-06-20 16:19:24 -0300

    adicionado segurança na tela de login
    Criado novos metodos de segurança na tela de login

--- TelaPrincipal/main.js
+++ TelaPrincipal/main.js
@@ -1,18 +1,10 @@
-// document.getElementById("perfil").addEventListener("click", () =>{
-
-//     var menu = document.getElementById("list_perfil");
-//     menu.classList.toggle("visivel");
+// document.getElementById("perfil").addEventListener("mouseover", () => {
+//     document.getElementById("list-perfil").style.display = "block";
 // });
 
-
-document.getElementById("perfil").addEventListener("mouseover", () => {
-    document.getElementById("list_perfil").style.display = "block";
-});
-
-document.getElementById("perfil").addEventListener("mouseout", () => {
-    document.getElementById("list_perfil").style.display = "none";
-});
-
+// document.getElementById("perfil").addEventListener("mouseout", () => {
+//     document.getElementById("list-perfil").style.display = "none";
+// });
 
 const swiper = new Swiper('.swiper', {
     speed: 400,
