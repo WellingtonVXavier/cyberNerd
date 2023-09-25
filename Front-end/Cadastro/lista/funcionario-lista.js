cc2bdcc8c3ce0f52cdaecd74d4658eada34a06b7
wellington.xavier <sidious.lords@gmail.com>
2023-09-25 15:45:28 -0300

    Editar dados
    Correção na edição de dados na tela de funcionarios

--- Front-end/Cadastro/lista/funcionario-lista.js
+++ Front-end/Cadastro/lista/funcionario-lista.js
@@ -2,7 +2,7 @@
 let funcionarios = [];
 
 function preencherTabela() {
-    console.table(funcionarios);
+    // console.table(funcionarios);
     const tbody = $("table tbody");
 
     funcionarios.forEach((itemFuncionario, index) => {
@@ -22,9 +22,17 @@ function preencherTabela() {
                 <td>${itemFuncionario.senha}</td>
                 <td>${itemFuncionario.confirmaSenha}</td>
                 <td>
-                    <i class="fa-solid fa-pen-to-square edit-icon pointer-icon" data-index="${index}">
+                    <i class="fa-solid fa-pen-to-square  edit-icon pointer-icon" style="cursor: pointer;" data-index="${index}">
                         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                        
                             <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/>
+                        </svg>
+                    </i>
+
+                    <i class="fa-solid fa-trash pointer-delete" style="cursor: pointer;">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                        
+                            <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
                         </svg>
                     </i>
                 </td>
@@ -80,7 +88,7 @@ function ativarEdicao(index) {
 
     const inputs = row.find("input");
     inputs.each((i, input) => {
-        const columnName = input.name.toLowerCase();
+        const columnName = input.name;
         console.log(`Input name: ${input.name}`);
         console.log(`Column name: ${columnName}`);
         console.log(`Funcionario:`, funcionario);
@@ -131,7 +139,7 @@ function salvarEdicao(index, id) {
     const editedFuncionario = {};
 
     row.find("input").each((i, input) => {
-        editedFuncionario[input.name.toLowerCase()] = input.value;
+        editedFuncionario[input.name] = input.value;
     });
 
     // Faça uma solicitação AJAX para atualizar o arquivo db.json
@@ -164,3 +172,24 @@ function salvarEdicao(index, id) {
         }
     });
 }
+
+// Adicione um evento de clique para o ícone de exclusão
+$("table").on("click", ".pointer-delete", function () {
+    const index = $(this).closest("tr").index(); // Obtém o índice da linha clicada
+    const id = funcionarios[index].id; // Obtém o ID do funcionário a ser excluído
+
+    // Faça uma solicitação AJAX para excluir o funcionário
+    $.ajax({
+        url: "http://localhost:3000/cadastro/" + id,
+        type: "DELETE",
+        success: () => {
+            // Remova a linha da tabela após a exclusão bem-sucedida
+            $("table tr").eq(index).remove();
+        },
+        error: (error) => {
+            alert("Ocorreu um erro ao excluir o funcionário");
+            console.log("error", error);
+        }
+    });
+});
+
--- Front-end/Login/login.js
+++ Front-end/Login/login.js
@@ -1,137 +1,101 @@
-var form = document.getElementById('form-login');
-var entrarButton = document.getElementById('#entrar');
-// var usuarioInput = document.getElementById('#usuario');
-// var senhaInput = document.getElementById('#senha');
-
-// form.addEventListener("submit", event => {
-//     event.preventDefault();
-
-//     const campos = {
-//         "usuario": usuarioInput.value,
-//         "senha": senhaInput.value
-//     };
-//     validarLogin(campos);
-// });
-
-// // Encontrando usuarios no banco: 
-// let usuariosCadastro =
-// {
-//     email: "",
-//     senha: ""
-// }
-
-// function callBackAcerto(result) {
-
-//     usuariosCadastro = result.map(itemfuncionario => {
-//         return {
-//             email: itemfuncionario.email,
-//             senha: itemfuncionario.senha
-//         }
-//     });
-// }
-
-// function submitLogin() {
-//     var modal = document.getElementById("cardModal");
-//     modal.style.display = "none";
-//     console.log("teste");
-// }
-
-// function callBackInvalido(error) {
-//     alert("Ocorreu um erro na Atualização")
-//     console.log("error", error)
-// }
-
-// let configRequest = {
-//     "url": "http://localhost:3000/cadastro",
-//     "type": "GET",
-//     success: (result) => callBackAcerto(result),
-//     error: (error) => callBackInvalido(error)
-// }
-
-// $.ajax(configRequest)
-
-// function findUsuario(usuarioLogin) {
-//     return usuariosCadastro.find(usuarioEmail => usuarioEmail.usuario === usuarioLogin)
-// }
-
-// function validarLogin(campos, event) {
-//     var usuarioLogin = campos.usuario;
-//     var senhaLogin = campos.senha;
-//     let usuarioEncontrado = findUsuario(usuarioLogin)
-
-//     if (usuarioEncontrado) {
-//         if (validaSenha(usuarioEncontrado, senhaLogin)) {
-//             window.location.href = '../TelaPrincipal/TelaInicial.html';
-//         } else {
-//             ValidarMensagem('senhaLogine/ou Usuário inválidos. Tente novamente.');
-//             abrirModal()
-//         }
-//     } else {
-//         ValidarMensagem('Usuário não cadastrado.');
-//         abrirModal()
+// document.addEventListener("DOMContentLoaded", function () {
+//     const modal = document.querySelector(".modal");
+//     const usuarioInput = document.getElementById("usuario");
+//     const senhaInput = document.getElementById("senha");
+  
+//     // Função para mostrar o modal com a mensagem de erro
+//     function mostrarModal(mensagem) {
+//       const modalContent = modal.querySelector(".modal");
+//       modalContent.querySelector("p").textContent = mensagem;
+//       modal.style.display = "block";
 //     }
-// }
-
-// function validaSenha(usuarioEncontrado, senha) {
-//     return usuarioEncontrado.senhaLogin === senha;
-// }
-
-// function ValidarMensagem(e) {
-//     const novo_elemeto = document.createElement("p");
-//     novo_elemeto.classList.add("mensagem");
-
-//     novo_elemeto.innerHTML += e;
-//     fundo.appendChild(novo_elemeto);
-// }
-
-// function abrirModal() {
-//     var menu = document.getElementById("modalCard");
-//     var modalLogin = document.getElementById("content");
-
-//     menu.classList.toggle("oculto");
-//     modalLogin.classList.toggle("ocultar");
-// }
-
-// document.getElementById("Fechar").addEventListener("click", () => {
-//     var menu = document.getElementById("modalCard");
-//     var recado = document.querySelectorAll(".mensagem");
-//     var modalLogin = document.getElementById("content");
-
-//     usuarioInput.value = "";
-//     senhaInput.value = "";
-
-//     recado.forEach((recado) => {
-//         recado.parentNode.removeChild(recado);
-//     });
-
-//     menu.classList.toggle("oculto");
-//     modalLogin.classList.toggle("modal-content");
-// });
-
-document.getElementById("entrar").addEventListener("click", function () {
-    const usuarioInput = document.getElementById("usuario").value;
-    const senhaInput = document.getElementById("senha").value;
-
-    // Carrega os dados do arquivo db.json
-    fetch("db.json")
-        .then(response => response.json())
-        .then(data => {
-            // Verifica se o email e a senha correspondem aos dados no arquivo
-            const usuarioEncontrado = data.cadastro.find(user => user.email === usuarioInput && user.senha === senhaInput);
-
-            if (usuarioEncontrado) {
-                // Login bem-sucedido, redirecione ou execute outras ações necessárias
-                alert("Login bem-sucedido!");
+  
+//     // Função para fechar o modal
+//     function fecharModal() {
+//       modal.style.display = "none";
+//     }
+  
+//     // Função para verificar o usuário e senha
+//     function validarLogin() {
+//       const usuario = usuarioInput.value.toLowerCase();
+//       const senha = senhaInput.value;
+  
+//       // Carregar o arquivo JSON (db.json) e realizar a validação
+//       fetch("../../Back-end/BANCO/db.json")
+//         .then((response) => response.json())
+//         .then((data) => {
+//           const cadastros = data.cadastro;
+  
+//           const usuarioEncontrado = cadastros.find(
+//             (cadastro) => cadastro.email.toLowerCase() === usuario
+//           );
+  
+//           if (!usuarioEncontrado) {
+//             // Usuário não cadastrado
+//             mostrarModal("Usuário não cadastrado!");
+//           } else if (usuarioEncontrado.senha !== senha) {
+//             // Senha incorreta
+//             mostrarModal("Senha inválida!");
+//           } else {
+//             // Login bem-sucedido, redirecionar para a tela inicial
+//             window.location.href = "../TelaPrincipal/TelaInicial.html";
+//           }
+//         });
+//     }
+  
+//     // Associar a função de validação ao clique no botão "Entrar"
+//     const entrarButton = document.getElementById("entrar");
+//     entrarButton.addEventListener("click", validarLogin);
+  
+//     // Associar a função de fechar modal ao clique no botão "Fechar"
+//     const fecharButton = document.getElementById("Fechar");
+//     fecharButton.addEventListener("click", fecharModal);
+//   });
+  
+  
+  
+  document.addEventListener("DOMContentLoaded", function () {
+      const modal = document.getElementById("cardModal");
+      const usuarioInput = document.getElementById("usuario");
+      const senhaInput = document.getElementById("senha");
+    
+      // Função para verificar o usuário e senha
+      function validarLogin() {
+        const usuario = usuarioInput.value.toLowerCase(); // Converter o email para minúsculas
+        const senha = senhaInput.value;
+    
+        // Carregar o arquivo JSON (db.json) e realizar a validação
+        fetch("../../Back-end/BANCO/db.json")
+          .then((response) => response.json())
+          .then((data) => {
+            const cadastros = data.cadastro;
+    
+            const usuarioEncontrado = cadastros.find(
+              (cadastro) => cadastro.email.toLowerCase() === usuario
+            );
+    
+            if (!usuarioEncontrado) {
+              // Usuário não cadastrado
+              modal.querySelector("p").textContent = "Usuário não cadastrado!";
+              modal.style.display = "block";
+            } else if (usuarioEncontrado.senha !== senha) {
+              // Senha incorreta
+              modal.querySelector("p").textContent = "Senha inválida!";
+              modal.style.display = "block";
             } else {
-                // Mostra o modal de informações se o usuário não for encontrado
-                document.getElementById("#cardModal").style.display = "block";
+              // Login bem-sucedido, redirecionar para a tela inicial
+              window.location.href = "../TelaPrincipal/TelaInicial.html";
             }
-        })
-        .catch(error => {
-            console.error("Erro ao carregar o arquivo db.json:", error);
-        });
-});
-
-document.getElementById("Fechar").addEventListener("click", function () {
-    document.getElementById("#cardModal").style.display = "none";
-});
+          });
+      }
+    
+      // Associar a função de validação ao clique no botão "Entrar"
+      const entrarButton = document.getElementById("entrar");
+      entrarButton.addEventListener("click", validarLogin);
+    
+      // Fechar o modal ao clicar no botão "Fechar"
+      const fecharButton = document.getElementById("Fechar");
+      fecharButton.addEventListener("click", function () {
+        modal.style.display = "none";
+      });
+    });
