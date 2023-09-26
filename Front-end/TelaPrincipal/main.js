20da816a905bd71433ababc2e70e21cf2c8f6eb0
wellington.xavier <sidious.lords@gmail.com>
2023-09-26 18:05:21 -0300

    novos ajustes
    ainda falta a validação para aparecer o nome do usuário logado na tela inicial

--- Front-end/TelaPrincipal/main.js
+++ Front-end/TelaPrincipal/main.js
@@ -8,3 +8,27 @@ const swiper = new Swiper('.swiper', {
         type: 'bullets',
     }
 });
+
+// Função para buscar o nome do usuário do arquivo JSON
+function carregarNomeUsuario() {
+    //Faça uma solicitação AJAX para buscar os dados do usuário do arquivo JSON (assumindo que você tenha o arquivo db.json no mesmo diretório)
+    fetch("../../Back-end/BANCO/db.json")
+        .then(response => response.json())
+        .then(cadastros => {
+            // Obtenha o nome do usuário dos dados
+            const usuarioLogado =  cadastros.nome; // Substitua "nome" pelo campo correto em seu arquivo JSON
+            console.table(usuarioLogado);
+
+            // Atualize o conteúdo da tag <div> com o nome do usuário
+            document.getElementById('nome-usuario').innerHTML = `Olá, ${usuarioLogado}`;
+        })
+        .catch(error => {
+            console.error('Erro ao carregar dados do usuário:', error);
+        });
+}
+
+// Chame a função para carregar o nome do usuário quando a página carregar
+window.addEventListener('load', carregarNomeUsuario);
+
+
+
